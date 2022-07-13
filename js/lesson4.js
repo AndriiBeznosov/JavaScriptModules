dc44624ac8ded8a138ea73f7b180489e339df5d7
Andrii Beznosov <andrey301288@gmail.com>
2022-07-13 19:21:52 +0300

    +

--- js/lesson4.js
+++ js/lesson4.js
@@ -10,7 +10,7 @@
 // const clients = ['Mango', 'Ajax', 'Poly'];
 
 // for (let i = 0; i < clients.length; i += 1) {
-//   console.log(clients[i]);
+//   console.log(i);
 // }
 // const clients = ['Mango', 'Ajax', 'Poly'];
 
@@ -507,24 +507,24 @@
 // data.updateRating('-20000-');
 // console.log(data);
 // console.log('Количество треков:', data.getTrack());
-let totalFeedback = 0;
-const feedback = {
-  good: 5,
-  neutral: 10,
-  bad: 7,
-};
-const keys = Object.keys(feedback);
-console.log(keys);
+// let totalFeedback = 0;
+// const feedback = {
+//   good: 5,
+//   neutral: 10,
+//   bad: 7,
+// };
+// const keys = Object.keys(feedback);
+// console.log(keys);
 
-for (const key of keys) {
-  console.log(key);
-  console.log(feedback[key]);
+// for (const key of keys) {
+//   console.log(key);
+//   console.log(feedback[key]);
 
-  totalFeedback += feedback[key];
-}
-// Number Boolean Array Object
+//   totalFeedback += feedback[key];
+// }
+// // Number Boolean Array Object
 
-console.log('Результат с обьекта:', totalFeedback);
+// console.log('Результат с обьекта:', totalFeedback);
 
 // function invert(array) {
 //   for (let i = 0; i < array.length; i++) {
@@ -553,3 +553,447 @@ console.log('Результат с обьекта:', totalFeedback);
 // console.log(invert([]));
 // console.log(invert([0])); // [];
 //Можно предположить, что все значения являются целыми числами. Не изменяйте входной массив/список.
+// let create = 0;
+// function repeatStr(n, s) {
+//   let create = s.repeat(n);
+// for (let i = 0; i < n; i += 1) {
+//   i += s;
+//   create += i;
+//   console.log(i);
+//   // console.log(create);
+// }
+//   console.log(create);
+// }
+// // console.log();
+// repeatStr(3, '*'); //'***';
+// repeatStr(5, '#'); //'#####';
+// repeatStr(2, 'ha '); //'ha ha ';
+
+// const book = {
+//   title: 'The Last Kingdom',
+//   author: 'Bernard Cornwell',
+//   genres: ['historical prose', 'adventure'],
+//   isPublic: true,
+//   rating: 8.38,
+// };
+
+// const propKey = 'author';
+// // console.log(propKey);
+// const bookAuthor = book[propKey];
+// // console.log(bookAuthor); // 'Bernard Cornwell'
+
+// console.log(book);
+// const animal = {
+//   legs: 4,
+// };
+// const dog = Object.create(animal);
+// dog.name = 'Манго';
+
+// console.log(dog); // {name: 'Манго'}
+// console.log(dog.name); // 'Манго'
+// console.log(dog.legs); // 4
+// function countProps(object) {
+//   let propCount = 0;
+// Change code below this line
+// for (let key in object) {
+//   console.log(object);
+
+// }
+//   const keys = Object.keys(object);
+//   console.log(keys.length);
+//   // Change code above this line
+//   // return propCount;
+// }
+
+// countProps({});
+// countProps({ name: 'Mango', age: 2 });
+// countProps({ mail: 'poly@mail.com', isOnline: true, score: 500 });
+// const apartment = {
+//   descr: 'Spacious apartment in the city center',
+//   rating: 4,
+//   price: 2153,
+// };
+// const values = [];
+
+// const keys = Object.keys(apartment);
+// const key = Object.values(apartment);
+// for (let i of key) {
+//   values.push(i);
+// }
+// // for (let key of Object.keys([apartment])) {
+// //   values.push(key);
+// console.log(key);
+// // }
+
+// console.log(values);
+// console.log(keys);
+
+// function countProps(object) {
+//   let propCount = 0;
+//   let key = Object.keys(object);
+//   propCount = key.length;
+//   // for (const key in object) {
+//   //   if (object.hasOwnProperty(key)) {
+//   //     propCount += 1;
+//   //   }
+//   // }
+
+//   return propCount;
+// }
+// // console.log(propCount);
+// console.log(countProps({})); // возвращает 0
+// console.log(countProps({ name: 'Mango', age: 2 })); // возвращает 2
+// console.log(countProps({ mail: 'poly@mail.com', isOnline: true, score: 500 })); // возвращает 3
+// const colors = [
+//   { hex: '#f44336', rgb: '244,67,54' },
+//   { hex: '#2196f3', rgb: '33,150,243' },
+//   { hex: '#4caf50', rgb: '76,175,80' },
+//   { hex: '#ffeb3b', rgb: '255,235,59' },
+// ];
+// const hexColors = [];
+// const rgbColors = [];
+// // Change code below this line
+
+// for (let i of colors) {
+//   hexColors.push(i.hex);
+//   rgbColors.push(i.rgb);
+// }
+// console.log(hexColors);
+// console.log(rgbColors);
+// const products = [
+//   { name: 'Radar', price: 1300, quantity: 4 },
+//   { name: 'Scanner', price: 2700, quantity: 3 },
+//   { name: 'Droid', price: 400, quantity: 7 },
+//   { name: 'Grip', price: 1200, quantity: 9 },
+//   { name: 'Engine', price: 1200000, quantity: 9 },
+// ];
+
+// function getProductPrice(productName) {
+//   for (let calsSum of products) {
+//     if (calsSum.name.includes(productName)) {
+//       return calsSum.price;
+//     }
+//   }
+//   return null;
+// }
+// console.log(getProductPrice('Radar')); // возвращает 1300.
+// console.log(getProductPrice('Grip')); // возвращает 1200.
+// console.log(getProductPrice('Scanner')); // возвращает 2700.
+// console.log(getProductPrice('Droid')); // возвращает 400.
+// console.log(getProductPrice('Engine')); // возвращает null.
+// const products = [
+//   { name: 'Radar', price: 1300, quantity: 4 },
+//   { name: 'Scanner', price: 2700, quantity: 3 },
+//   { name: 'Droid', price: 400, quantity: 7 },
+//   { name: 'Grip', price: 1200, quantity: 9 },
+// ];
+// let cals = [];
+// function getAllPropValues(productName) {
+//   let cals = [];
+//   for (let calsSum of products) {
+//     if (calsSum.hasOwnProperty([productName])) {
+//       cals.push(calsSum[productName]);
+//     }
+//   }
+//   return cals;
+// }
+
+// console.log(getAllPropValues('name')); // возвращает ["Radar", "Scanner", "Droid", "Grip"]
+// console.log(getAllPropValues('quantity')); // возвращает [4, 3, 7, 9]
+// console.log(getAllPropValues('price')); // возвращает [1300, 2700, 400, 1200]
+// console.log(getAllPropValues('category')); // возвращает []
+//Напиши функцию calculateTotalPrice(productName) которая принимает один параметр productName - название товара. Функция должна вернуть общую стоимость (цена * количество) товара с таким именем из массива products.
+
+// const products = [
+//   { name: 'Radar', price: 1300, quantity: 4 },
+//   { name: 'Scanner', price: 2700, quantity: 3 },
+//   { name: 'Droid', price: 400, quantity: 7 },
+//   { name: 'Grip', price: 1200, quantity: 9 },
+// ];
+
+// let cals = 0;
+// function calculateTotalPrice(productName) {
+//   for (let calsSum of products) {
+//     if (Object.values(calsSum).includes(productName)) {
+//       console.log(`${calsSum.price} умножить ${calsSum.quantity}`);
+//       return calsSum.price * calsSum.quantity;
+//     }
+//   }
+//   return cals;
+// }
+
+// console.log(calculateTotalPrice('Blaster')); // возвращает 0
+// console.log(calculateTotalPrice('Radar')); // возвращает 5200
+// console.log(calculateTotalPrice('Droid')); // возвращает 2800
+// console.log(calculateTotalPrice('Grip')); // возвращает 10800
+// console.log(calculateTotalPrice('Scanner')); // возвращает 8100
+// const book = {
+//   title: 'The Last Kingdom',
+//   author: 'Bernard Cornwell',
+//   genres: ['historical prose', 'adventure'],
+//   rating: 8.38,
+// };
+
+// for (const key in book) {
+//   // Ключ
+//   console.log(key);
+// Значение свойства с таким ключом
+// console.log(book[key]);
+// }
+// const animal = {
+//   legs: 4,
+// };
+// const dog = Object.create(animal);
+// dog.name = 'Манго';
+
+// console.log(dog); // {name: 'Манго'}
+// console.log(dog.name); // 'Манго'
+// console.log(dog.legs); // 4
+// const book = {
+//   title: 'The Last Kingdom',
+//   author: 'Bernard Cornwell',
+//   genres: ['historical prose', 'adventure'],
+//   rating: 8.38,
+// };
+// const keys = Object.keys(book);
+
+// for (const key of keys) {
+//   // Ключ
+//   console.log(key);
+//   // Значение свойства
+//   console.log(book[key]);
+// }
+// function lovefunc(flower1, flower2) {
+//   // moment of truth
+//   return flower1 === flower2 ? false : true
+
+// }
+
+// console.log(lovefunc(1, 4)); // true
+// console.log(lovefunc(2, 2)); //false
+// console.log(lovefunc(858, 208)); //true)
+// console.log(lovefunc(0, 0)); //false
+// const firstBook = {
+//   title: 'Последнее королевство',
+//   coverImage:
+//     'https://images-na.ssl-images-amazon.com/images/I/51b5YG6Y1rL.jpg',
+// };
+
+// const {
+//   title: firstTitle,
+//   coverImage: firstCoverImage = 'https://via.placeholder.com/640/480',
+// } = firstBook;
+
+// console.log(firstTitle); // Последнее королевство
+// console.log(firstCoverImage); // https://images-na.ssl-images-amazon.com/images/I/51b5YG6Y1rL.jpg
+
+// const secondBook = {
+//   title: 'Сон смешного человека',
+// };
+
+// const {
+//   title: secondTitle,
+//   coverImage: secondCoverImage = 'https://via.placeholder.com/640/480',
+// } = secondBook;
+
+// console.log(secondTitle); // Сон смешного человека
+// console.log(secondCoverImage); // https://via.placeholder.com/640/480
+
+////////////////////////////////////////////////////////
+// const cart = {
+//   items: [],
+//   getItems() {
+//     return this.items;
+//   },
+//   add(product) {
+//     this.items.push(product);
+//   },
+//   remove(productName) {
+//     for (let i = 0; i < this.items.length; i += 1) {
+//       //   console.log(this.items[i]);
+//       if (productName === this.items[i]) {
+//         console.log(`Нашли продукт: ${productName}`);
+//       }
+//     }
+//     // for (let item of this.items) {
+//     //   console.log(item);
+
+//     //   if (productName === item.name) {
+//     //     console.log(`Нашли продукт: ${productName}`);
+//     //   }
+//     // }
+//   },
+//   clear() {},
+//   countTotalPrice() {},
+//   increaseQuantity(productName) {},
+//   decreaseQuantity(productName) {},
+// };
+
+// console.table(cart.getItems());
+
+// cart.add({ name: 'яблоко', price: 22 });
+// cart.add({ name: 'груша', price: 85 });
+// cart.add({ name: 'диня', price: 100 });
+// cart.add({ name: 'арбуз', price: 62 });
+// cart.add({ name: 'слива', price: 40 });
+// cart.add({ name: 'абрикос', price: 105 });
+// cart.add({ name: 'Черешеня', price: 160 });
+// cart.add({ name: 'Черешеня', price: 160 });
+
+// console.table(cart.getItems());
+
+// cart.remove('яблоко');
+///////////////////////////////////////////////////
+// const haystack_1 = [
+//   '3',
+//   '123124234',
+//   undefined,
+//   'needle',
+//   'world',
+//   'hay',
+//   2,
+//   '3',
+//   true,
+//   false,
+// ];
+// const haystack_2 = [
+//   '283497238987234',
+//   'a dog',
+//   'a cat',
+//   'some random junk',
+//   'a piece of hay',
+//   'needle',
+//   'something somebody lost a while ago',
+// ];
+// const haystack_3 = [
+//   1, 2, 3, 4, 5, 6, 7, 8, 8, 7, 5, 4, 3, 4, 5, 6, 67, 5, 5, 3, 3, 4, 2, 34, 234,
+//   23, 4, 234, 324, 324, 1, 2, 3, 4, 5, 5, 6, 5, 4, 32, 3, 45, 54,
+// ];
+// function findNeedle(haystack) {
+//   const array = 'needle';
+//   for (let i = 0; i < haystack.length; i += 1) {
+//     if (haystack[i] === array) {
+//       return `found the needle at position ${[i]}`;
+//     }
+//   }
+
+//   return "Your function didn't return anything";
+// }
+
+// //--> undefined -->"Your function didn't return anything",
+// console.log(findNeedle(haystack_1)); //'found the needle at position 3'
+// console.log(findNeedle(haystack_2)); //'found the needle at position 5'
+// console.log(findNeedle(haystack_3)); //'found the needle at position 30'
+
+// const temps = [14, -4, 25, 8, 11];
+
+// // В консоли будет массив
+// console.log(temps);
+// // ❌ Так не сработает, потому что передаём целый массив
+// console.log(Math.max(temps)); // NaN
+
+// console.log(...temps);
+// // ✅ Распылим коллекцию элементов как отдельные аргументы
+// console.log(Math.max(...temps)); // 25
+// const user = {
+//   name: 'Jacques Gluke',
+//   tag: 'jgluke',
+//   stats: {
+//     followers: 5603,
+//     views: 4827,
+//     likes: 1308,
+//   },
+// };
+
+// const {
+//   name,
+//   tag,
+//   stats: { followers, views: userViews, likes: userLikes = 0 },
+// } = user;
+
+// console.log(name); // Jacques Gluke
+// console.log(tag); // jgluke
+// console.log(followers); // 5603
+// console.log(userViews); // 4827
+// console.log(userLikes); // 1308
+// Change code below this line
+// function addOverNum(firstNumber, ...args) {
+//   let total = 0;
+//   // for (const arg of args) {
+
+//   //   // total += arg;
+//   // }
+//   for (let i = 0; i < args.length; i += 1) {
+//     if (args[i] > firstNumber) {
+//       total += args[i];
+//     }
+//   }
+
+//   return total;
+//   // Change code above this line
+// }
+
+// console.log(addOverNum(50, 15, 27)); // возвращает 0
+// console.log(addOverNum(10, 12, 4, 11, 48, 10, 8)); // возвращает 71
+// console.log(addOverNum(15, 32, 6, 13, 19, 8)); // возвращает 51
+// console.log(addOverNum(20, 74, 11, 62, 46, 12, 36)); // возвращает 218
+// function removeChar(str) {
+//   const remove = str.slice(1, -1);
+//   return remove;
+// }
+
+// console.log(removeChar('country')); // 'ountr');
+// console.log(removeChar('person')); // 'erso');
+// console.log(removeChar('place')); // 'lac');
+// console.log(removeChar('ooopsss')); // 'oopss');
+
+// function isDivideBy(numbers, a, b) {
+//   return numbers / a === b;
+// }
+// function isDivideBy(n, a, b) {
+//   let num = n / a;
+
+//   return num % 2 === 0;
+// }
+// function isDivideBy(numbers, a, b) {
+//   const num1 = numbers / a;
+//   return num1 === b && Number.isInteger(num1);
+// }
+
+// let isDivideBy = (number, a, b) => {
+//   if (number % a === 0 && number % b === 0) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// };
+
+// console.log(isDivideBy(-12, 2, -6)); // true);
+// console.log(isDivideBy(-12, 2, -5)); // false);
+// console.log(isDivideBy(45, 1, 6)); // false);
+// console.log(isDivideBy(45, 5, 15)); // true);
+// console.log(isDivideBy(4, 1, 4)); // true);
+// console.log(isDivideBy(3156, 2, 6)); // true);
+
+// function find_average(array) {
+//   let total = 0;
+//   let tot = 0;
+//   for (let i = 0; i < array.length; i += 1) {
+
+//     console.log(array[i]);
+// return (total += array.length);
+// return total / array.length - 1 + 1;
+//   }
+//   // return 0;
+// }
+
+// function find_average(array) {
+//   let sum = 0;
+//   for (let i = 0; i < array.length; i += 1) {
+//     sum += array[i];
+//     console.log(sum / array.length - 1);
+//   }
+// }
+// console.log(find_average([1, 1, 1])); //, 1);
+// console.log(find_average([1, 2, 3])); //, 2);
+// console.log(find_average([1, 2, 3, 4])); //, 2.5);
---
+++ js/lesson6.js
@@ -0,0 +1,383 @@
+/**
+ * Example 5 - Комплексные задачи
+ * Напиши скрипт управления личным кабинетом интернет банка.
+ * Есть объект account в котором необходимо реализовать методы для работы с балансом и историей транзакций.
+ */
+/////////////////////////////////
+/*
+//  * Типов транзацкий всего два.
+//  * Можно положить либо снять деньги со счета.
+//  */
+// const Transaction = {
+//   DEPOSIT: 'deposit',
+//   WITHDRAW: 'withdraw',
+// };
+
+// /*
+//  * Каждая транзакция это объект со свойствами: id, type и amount
+//  */
+
+// const account = {
+//   // Текущий баланс счета
+//   balance: 0,
+
+//   // История транзакций
+//   transactions: [],
+
+//   /*
+//    * Метод создает и возвращает объект транзакции.
+//    * Принимает сумму и тип транзакции.
+//    */
+//   createTransaction(amount, type) {
+//     return {
+//       id: this.transactions.length + 1,
+//       amount,
+//       type,
+//     };
+
+//     // return {
+//     //   id: this.transactions.length + 1,
+//     //   amount,
+//     //   type,
+//     // };
+//   },
+
+//   /*
+//    * Метод отвечающий за добавление суммы к балансу.
+//    * Принимает сумму танзакции.
+//    * Вызывает createTransaction для создания объекта транзакции
+//    * после чего добавляет его в историю транзакций
+//    */
+//   deposit(amount) {
+//     const transition = this.createTransaction(amount, Transaction.DEPOSIT);
+//     this.transactions.push(transition);
+//     this.balance += amount;
+//     // const transaction = this.createTransaction(amount, Transaction.DEPOSIT);
+//     // this.transactions.push(transaction);
+//     // this.balance += amount;
+//   },
+
+//   /*
+//    * Метод отвечающий за снятие суммы с баланса.
+//    * Принимает сумму танзакции.
+//    * Вызывает createTransaction для создания объекта транзакции
+//    * после чего добавляет его в историю транзакций.
+//    *
+//    * Если amount больше чем текущий баланс, выводи сообщение
+//    * о том, что снятие такой суммы не возможно, недостаточно средств.
+//    */
+//   withdraw(amount) {
+//     if (amount > this.balance) return 'недостаточно средств';
+
+//     // const transaction = this.createTransaction(amount, Transaction.WITHDRAW);
+
+//     // this.transactions.push(transaction);
+//     // this.balance -= amount;
+//   },
+
+//   /*
+//    * Метод возвращает текущий баланс
+//    */
+//   getBalance() {
+//     // return this.balance;
+//   },
+
+//   /*
+//    * Метод ищет и возвращает объект транзации по id
+//    */
+//   getTransactionDetails(id) {
+//     // let tr = null;
+//     // for (let transaction of this.transactions) {
+//     //   if (transaction.id === id) {
+//     //     tr = { ...transaction };
+//     //     break;
+//     //   }
+//     // }
+//     // return tr;
+//   },
+
+//   /*
+//    * Метод возвращает количество средств
+//    * определенного типа транзакции из всей истории транзакций
+//    */
+//   getTransactionTotal(type) {
+//     // let sum = 0;
+//     // for (let transaction of this.transactions) {
+//     //   if (transaction.type === type) {
+//     //     sum += transaction.amount;
+//     //   }
+//     // }
+//     // return sum;
+//   },
+// };
+///////////////////////////////
+// createTransaction(1000, 'deposit');
+// deposit(5000);
+//Создайте массив styles с элементами «Джаз» и «Блюз».
+//Добавьте «Рок-н-ролл» в конец.
+//Замените значение «Блюз» на «Классика».
+//Удалите первый элемент массива и выведите его в консоль.
+//Вставьте «Рэп» и «Регги» в начало массива.
+
+// const styles = ['Джаз', 'Блюз'];
+// styles.push('Рок-н-ролл');
+// console.log(styles);
+// styles[1] = 'Классика';
+// console.log(styles);
+// console.log(styles.shift());
+// styles.unshift('Рэп', 'Регги');
+// console.log(styles);
+
+//Напишите функцию caculculateAverage()
+//которая принимает произвольное количество
+//аргументов и возвращает их среднее значение.
+//Добавить проверку, что аргументы это числа.
+
+// function caculculateAverage(...arrgs) {
+//   let total = 0;
+//   let sum = arrgs.length;
+//   for (let i = 0; i < arrgs.length; i += 1) {
+//     if (isNaN(arrgs[i])) {
+//       sum -= 1;
+//       continue;
+//     }
+//     total += arrgs[i];
+//   }
+//   return total / sum;
+// }
+// console.log(caculculateAverage(1, 2, 3, 5, 'nan', 'sdlgns'));
+
+//Напишите функцию findTheColor()
+//которая принимает название цвета
+//определяет его наличие в массиве
+//если цвет есть, усталавливет его как фон документа
+//если цвета нет выводит console.error('Такой цвет не найден');
+//и устанавливает цвет документа красный
+// document.body.style.background = 'red';
+//const colors = ['blue', 'yellow', 'olive', 'fuchsia', 'lime', 'aqua', 'maroon']
+
+//Напишите скрипт который заменяет регистр каждого символа
+//в стороке на противоположный
+//Например 'JavaScript' должен вернуть 'jAVAsCRIPT'
+
+//Напиши скрипт, который для объекта user,
+//последовательно:
+//1 добавит поле mood со значением 'happy'
+//2 заменит hobby на 'skydiving'
+//3 заменит значение premium на false
+//4 выводит содержимое объекта users в фортмате
+//ключ:значение используя Object.keys() и for...of
+// const user = {
+//   name: "John",
+//   age: 20,
+//   hobby: "tenis",
+//   premium: true,
+// };
+
+//Создайте объект calculator с тремя методами
+//read(a, b) - принимает два аргумента и сохраняет их
+//как свойства объекта
+//sum() возвращает сумму сохраненных значений
+//mult() перемножает сохраненные значения и возвращает результат
+
+///////////////////////////////////////
+
+// Функция findMatches() принимает произвольное количество аргументов. Первым аргументом всегда будет массив чисел, а остальные аргументы будут просто числами.
+
+// Дополни код функции так, чтобы она возвращала новый массив matches, в котором будут только те аргументы, начиная со второго, которые есть в массиве первого аргумента.
+
+// Например, findMatches([1, 2, 3, 4, 5], 1, 8, 2, 7) должна вернуть массив [1, 2], потому что только они есть в массиве первого аргумента.
+
+// function findMatches(mas, ...args) {
+//   const matches = [];
+//   //   console.log('mas:', mas);
+//   //   console.log('a:', args);
+//   for (let arg of args) {
+//     // console.log('arg:', arg);
+//     if (mas.includes(arg)) matches.push(arg);
+//   }
+
+//   return matches;
+// }
+
+// console.log(findMatches([1, 2, 3, 4, 5], 1, 8, 2, 7)); // возвращает [1, 2]
+// console.log(findMatches([4, 89, 17, 36, 2], 8, 17, 89, 27, 2)); // возвращает [17, 89, 2]
+// console.log(findMatches([10, 24, 41, 6, 9, 19], 24, 11, 9, 23, 41)); // возвращает [24, 9, 41]
+// console.log(findMatches([63, 11, 8, 29], 4, 7, 16)); // возвращает []
+
+// Дополни метод updateBook(oldName, newName) так, чтобы он изменял название книги с oldName на newName в свойстве books. Используй indexOf() для того, чтобы найти нужный элемент массива, и splice() для того чтобы заменить этот элемент
+
+// const bookShelf = {
+//   books: ['The last kingdom', 'Haze', 'The guardian of dreams'],
+//   updateBook(oldName, newName) {
+//     const ind = this.books.indexOf(oldName);
+//     this.books.splice(ind, 1, newName);
+//     return this.books;
+//   },
+// };
+
+// console.log(bookShelf.updateBook('Haze', 'Dungeon chronicles')); // значение свойства books это массив ["The last kingdom", "Dungeon chronicles", "The guardian of dreams"]
+
+// console.log(bookShelf.updateBook('The last kingdom', 'Dune')); // значение свойства books это массив ["Dune", "Haze", "The guardian of dreams"]
+
+// const atTheOldToad = {
+//   potions: [
+//     { name: 'Speed potion', price: 460 },
+//     { name: 'Dragon breath', price: 780 },
+//     { name: 'Stone skin', price: 520 },
+//   ],
+
+//   getPotions() {
+//     return this.potions;
+//   },
+
+//   addPotion(newPotion) {
+//     for (let potion of this.potions) {
+//       if (potion.name === newPotion.name) {
+//         return `Error! Potion ${newPotion.name} is already in your inventory!`;
+//       }
+//     }
+//     this.potions.push(newPotion);
+//     return this.potions;
+//   },
+//   ////////////////////////////////
+//   removePotion(potionName) {
+//     for (let i = 0; i < this.potions.length; i += 1) {
+
+//       if (this.potions[i].name === potionName) {
+//         return this.potions.splice(i, 1);
+//       }
+//     }
+
+//     return `Potion ${potionName} is not in inventory!`;
+//   },
+//   ///////////////////////////////////////////
+//   //   updatePotionName(oldName, newName) {
+//   //     const potionIndex = this.potions.indexOf(oldName);
+
+//   //     if (potionIndex === -1) {
+//   //       return `Potion ${oldName} is not in inventory!`;
+//   //     }
+
+//   //     this.potions.splice(potionIndex, 1, newName);
+//   //   },
+// };
+
+// // console.log(atTheOldToad.getPotions());
+// // console.log(atTheOldToad.addPotion(newPotion));
+// console.log(atTheOldToad.potions);
+// console.log(atTheOldToad.addPotion({ name: 'Invisibility', price: 620 }));
+// console.log(atTheOldToad.removePotion('Dragon breath'));
+// console.log(split());
+// const hotel = {
+//   rang: 5,
+//   online: 10,
+//   folover: 'Andrii',
+// };
+// rang.raffff = 5;
+// console.log(hotel.rang);
+
+// const arrey = (a, b, c) => {
+//   const x = a + b / c;
+//   return x.toFixid(3);
+// };
+// console.log(arrey(20, 6, 7));
+
+// const numbers = [5, 10, 15, 90, 25];
+
+// Объявление функции
+// numbers.forEach(function (number, index) {
+//   console.log(`Индекс ${index}, значение ${number}`);
+// });
+// numbers.forEach((number, index) => {
+//   console.log(`Индекс ${index}, значение ${number}`);
+// });
+
+// const logMessage = (number, index) => {
+//   console.log(`Индекс ${index}, значение ${number}`);
+// };
+// numbers.forEach(logMessage);
+// const numbers = [1, 2, 3, 4, 5];
+// const filteredNumbers = [];
+
+// const students = [
+//   { name: 'Манго', score: 83 },
+//   { name: 'Поли', score: 59 },
+//   { name: 'Аякс', score: 37 },
+//   { name: 'Киви', score: 94 },
+//   { name: 'Хьюстон', score: 64 },
+// ];
+
+// const names = students.map(student => student.name);
+
+// console.log(names); // ['Манго', 'Поли', 'Аякс', 'Киви', 'Хьюстон']
+/////////////////////////////////////////
+// const validNumber = Number('51'); // 51
+// console.log(isNaN(validNumber)); // false
+
+// const invalidNumber = Number('qweqwe'); // NaN
+// console.log(isNaN(invalidNumber)); // true
+/////////////////////////////////////////
+
+// const pizzaPalace = {
+//   pizzas: ['Ultracheese', 'Smoked', 'Four meats'],
+//   order(pizzaName, onSuccess, onError) {
+//     if (this.pizzas.includes(pizzaName) === true) {
+//       return onSuccess(pizzaName);
+//     }
+//     return onError(pizzaName);
+
+//     // return this.pizzas.includes(pizzaName) ? onSuccess() : onError();
+//   },
+// };
+
+// function makePizza(pizzaName) {
+//   return `Your order is accepted. Cooking pizza ${pizzaName}.`;
+// }
+// // makePizza();
+// function onOrderError(error) {
+//   return `Error! There is no pizza with a name ${error} in the assortment. `;
+// }
+// // onOrderError();
+// console.log(pizzaPalace.order('Smoked', makePizza, onOrderError));
+// console.log(pizzaPalace.order('Four meats', makePizza, onOrderError));
+// console.log(pizzaPalace.order('Big Mike', makePizza, onOrderError));
+// console.log(pizzaPalace.order('Vienna', makePizza, onOrderError));
+// // console.log(pizzaPalace);
+// const planets = ['Earth', 'Mars', 'Venus', 'Jupiter'];
+// // Change code below this line
+// const planetsLengths = planets.map((a, b, c) => b);
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(planetsLengths);
+// const numbers = [17, 24, 82, 61, 36, 18, 47, 52, 73];
+// // Change code below this line
+
+// const evenNumbers = numbers.filter(a => a % 2 === 0);
+// const oddNumbers = numbers.filter(a => a % 2 !== 0);
+// console.log(evenNumbers);
+// console.log(oddNumbers);
+
+const books = [
+  {
+    title: 'The Last Kingdom',
+    author: 'Bernard Cornwell',
+    genres: ['adventure', 'history'],
+  },
+  {
+    title: 'Beside Still Waters',
+    author: 'Robert Sheckley',
+    genres: ['fiction', 'mysticism'],
+  },
+  {
+    title: 'Redder Than Blood',
+    author: 'Tanith Lee',
+    genres: ['horror', 'mysticism', 'adventure'],
+  },
+];
+// Change code below this line
+const allGenres = books.flatMap(a => a.genres);
+const uniqueGenres = allGenres.filter((a, b, c) => c.indexOf(a) === b);
+console.log(allGenres);
+console.log(uniqueGenres);
