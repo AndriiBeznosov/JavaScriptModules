a2ee976de1c657376619081c719d7d6463a3e2c3
Andrii Beznosov <andrey301288@gmail.com>
2022-07-16 00:15:53 +0300

    +

--- js/lesson6.js
+++ js/lesson6.js
@@ -359,25 +359,149 @@
 // console.log(evenNumbers);
 // console.log(oddNumbers);
 
+// const books = [
+//   {
+//     title: 'The Last Kingdom',
+//     author: 'Bernard Cornwell',
+//     genres: ['adventure', 'history'],
+//   },
+//   {
+//     title: 'Beside Still Waters',
+//     author: 'Robert Sheckley',
+//     genres: ['fiction', 'mysticism'],
+//   },
+//   {
+//     title: 'Redder Than Blood',
+//     author: 'Tanith Lee',
+//     genres: ['horror', 'mysticism', 'adventure'],
+//   },
+// ];
+// // Change code below this line
+// const allGenres = books.flatMap(a => a.genres);
+// const uniqueGenres = allGenres.filter((a, b, c) => c.indexOf(a) === b);
+// console.log(allGenres);
+// const a = [
+//   {
+//     name: 'Moore Hensley',
+//     email: 'moorehensley@indexia.com',
+//     eyeColor: 'blue',
+//     friends: ['Sharron Pace'],
+//     isActive: false,
+//     balance: 2811,
+//     gender: 'male',
+//     age: 37,
+//   },
+//   {
+//     name: 'Sharlene Bush',
+//     email: 'sharlenebush@tubesys.com',
+//     eyeColor: 'blue',
+//     friends: ['Briana Decker', 'Sharron Pace'],
+//     isActive: true,
+//     balance: 3821,
+//     gender: 'female',
+//     age: 34,
+//   },
+//   {
+//     name: 'Ross Vazquez',
+//     email: 'rossvazquez@xinware.com',
+//     eyeColor: 'green',
+//     friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
+//     isActive: false,
+//     balance: 3793,
+//     gender: 'male',
+//     age: 24,
+//   },
+//   {
+//     name: 'Elma Head',
+//     email: 'elmahead@omatom.com',
+//     eyeColor: 'green',
+//     friends: ['Goldie Gentry', 'Aisha Tran'],
+//     isActive: true,
+//     balance: 2278,
+//     gender: 'female',
+//     age: 21,
+//   },
+//   {
+//     name: 'Carey Barr',
+//     email: 'careybarr@nurali.com',
+//     eyeColor: 'blue',
+//     friends: ['Jordan Sampson', 'Eddie Strong'],
+//     isActive: true,
+//     balance: 3951,
+//     gender: 'male',
+//     age: 27,
+//   },
+//   {
+//     name: 'Blackburn Dotson',
+//     email: 'blackburndotson@furnigeer.com',
+//     eyeColor: 'brown',
+//     friends: ['Jacklyn Lucas', 'Linda Chapman'],
+//     isActive: false,
+//     balance: 1498,
+//     gender: 'male',
+//     age: 38,
+//   },
+//   {
+//     name: 'Sheree Anthony',
+//     email: 'shereeanthony@kog.com',
+//     eyeColor: 'brown',
+//     friends: ['Goldie Gentry', 'Briana Decker'],
+//     isActive: true,
+//     balance: 2764,
+//     gender: 'female',
+//     age: 39,
+//   },
+// ];
+
+// // const getFriends = users => {
+// //   return users
+// //     .flatMap(a => a.friends)
+// //     .filter((course, index, array) => {
+// //       return array.indexOf(course) === index;
+// //     });
+// // };
+// const getActiveUsers = users => {
+//   return users.filter(a => a.name && a.isActive === true).map(b => b.name);
+// };
+// // console.log(uniq);
+
+// console.log(getActiveUsers(a));
+// const players = [
+//   { name: 'Mango', playtime: 1270, gamesPlayed: 4 },
+//   { name: 'Poly', playtime: 469, gamesPlayed: 2 },
+//   { name: 'Ajax', playtime: 690, gamesPlayed: 3 },
+//   { name: 'Kiwi', playtime: 241, gamesPlayed: 1 },
+// ];
+// // Change code below this line
+
+// const totalAveragePlaytimePerGame = players.reduce((a, b) => {
+//   return a + b.playtime / b.gamesPlayed;
+// }, 0);
+// console.log(totalAveragePlaytimePerGame);
 const books = [
   {
     title: 'The Last Kingdom',
     author: 'Bernard Cornwell',
-    genres: ['adventure', 'history'],
+    rating: 8.38,
   },
   {
     title: 'Beside Still Waters',
     author: 'Robert Sheckley',
-    genres: ['fiction', 'mysticism'],
+    rating: 8.51,
   },
   {
-    title: 'Redder Than Blood',
-    author: 'Tanith Lee',
-    genres: ['horror', 'mysticism', 'adventure'],
+    title: 'The Dream of a Ridiculous Man',
+    author: 'Fyodor Dostoevsky',
+    rating: 7.75,
+  },
+  { title: 'Redder Than Blood', author: 'Tanith Lee', rating: 7.94 },
+  {
+    title: 'The Dreams in the Witch House',
+    author: 'Howard Lovecraft',
+    rating: 8.67,
   },
 ];
+const MIN_BOOK_RATING = 8;
 // Change code below this line
-const allGenres = books.flatMap(a => a.genres);
-const uniqueGenres = allGenres.filter((a, b, c) => c.indexOf(a) === b);
-console.log(allGenres);
-console.log(uniqueGenres);
+
+const names = books;
