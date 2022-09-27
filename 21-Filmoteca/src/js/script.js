4fe6e4332e490f9ef7aeb926c622099ddb039dc1
Andrii Beznosov <andrey301288@gmail.com>
2022-09-27 16:43:39 +0300

    script

--- 21-Filmoteca/src/js/script.js
+++ 21-Filmoteca/src/js/script.js
@@ -2,14 +2,46 @@ const refs = {
   backdrop: document.querySelector(".backdrop"),
   modal: document.querySelector(".modal"),
   btnClose: document.querySelector(".modal__close"),
-  itemCard: document.querySelector(".hero__item"),
-};
-console.log(refs.backdrop);
-console.log(refs.modal);
-console.log(refs.itemCard);
 
-refs.itemCard.addEventListener("click", removeClassHidden);
+  list: document.querySelector(".hero__list"),
+};
+
 refs.btnClose.addEventListener("click", addClassHidden);
+
+const API_KEY = "e761c0cc0c75e9e76bceba6c904ee9f8";
+const URL = "https://api.themoviedb.org/3";
+const API_URL = `${URL}/discover/movie?sort_by=popularity.desc&api_key=${API_KEY}`;
+const IMG = "https://image.tmdb.org/t/p/w500";
+
+async function getMovies() {
+  await fetch(API_URL)
+    .then((res) => {
+      return res.json();
+    })
+    .then((res) => {
+      render(res.results);
+    });
+}
+getMovies();
+
+function render(data) {
+  console.log(data);
+
+  const list = data
+    .map((item) => {
+      //   genre(item.original_title);
+      return `
+        <li class="hero__item">
+            <img src="${IMG}${item.poster_path}" alt="${item.title}" />
+             <h2 class="hero__title">${item.title}</h2>
+             <p class="hero__text">Drama, Action | 2020</p>
+            </li>`;
+    })
+    .join("");
+  refs.list.insertAdjacentHTML("beforeend", list);
+  (refs.itemCard = document.querySelector(".hero__item")),
+    refs.itemCard.addEventListener("click", removeClassHidden);
+}
 
 function removeClassHidden() {
   refs.backdrop.classList.remove("hidden");
@@ -17,3 +49,15 @@ function removeClassHidden() {
 function addClassHidden() {
   refs.backdrop.classList.add("hidden");
 }
+
+// function genre(id) {
+//   fetch(
+//     `https://api.themoviedb.org/3/company/${id}?api_key=e761c0cc0c75e9e76bceba6c904ee9f8`,
+//   )
+//     .then((res) => {
+//       return res.json();
+//     })
+//     .then((res) => {
+//       console.log(res);
+//     });
+// }
